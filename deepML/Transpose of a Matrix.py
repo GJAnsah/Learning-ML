c5c5d1778c999ead7cfcce7feca07de6919af9cc
Gilgal Ansah <ansahgilgaljustice@gmail.com>
2026-01-09 23:33:43 -0500

    Create Transpose of a Matrix.py

---
+++ deepML/Transpose of a Matrix.py
@@ -0,0 +1,17 @@
+#https://www.deep-ml.com/problems/2?from=Linear%20Algebra
+
+def transpose_matrix(a: list[list[int|float]]) -> list[list[int|float]]:
+    """
+    Transpose a 2D matrix by swapping rows and columns.
+    
+    Args:
+        a: A 2D matrix of shape (m, n)
+    
+    Returns:
+        The transposed matrix of shape (n, m)
+    """
+    # Your code here
+    result = []
+    for i in zip(*a):
+        result.append(list(i))
+    return result
